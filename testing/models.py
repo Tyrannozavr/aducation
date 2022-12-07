e136d97bfc7132421dfbabe8c779072bf8e1dc25
dmiv <dmitriy40647274@gmail.com>
2022-12-08 00:18:36 +0300

    the first version of the db design

--- testing/models.py
+++ testing/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Questions(models.Model):
-    question = models.CharField()
+
+class Answers(models.Model):
+    answer = models.CharField(max_length=255)
+
 
 class Tests(models.Model):
-    questions = models.ManyToManyField(Questions)
+    question = models.CharField(max_length=255)
+    variants = models.ManyToManyField(Answers, related_name='variants')
+    correct = models.ManyToManyField(Answers, related_name='correct')
 
 
 class Testing(models.Model):
+    number = models.IntegerField(auto_created=True)
     test = models.ForeignKey(Tests, on_delete=models.CASCADE)
-    correct = models.ManyToManyField(Questions)
+
+
+class Attempts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    testing = models.ForeignKey(Testing, on_delete=models.CASCADE)
+    answer = models.ManyToManyField(Answers)
