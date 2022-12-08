154e50f62cc3656384ee484fec85eab7913827f4
dmiv <dmitriy40647274@gmail.com>
2022-12-08 19:44:00 +0300

    changed db

--- testing/models.py
+++ testing/models.py
@@ -5,19 +5,30 @@ from django.contrib.auth.models import User
 class Answers(models.Model):
     answer = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.answer
+
 
 class Tests(models.Model):
     question = models.CharField(max_length=255)
     variants = models.ManyToManyField(Answers, related_name='variants')
     correct = models.ManyToManyField(Answers, related_name='correct')
 
+    def __str__(self):
+        return self.question
+
 
 class Testing(models.Model):
-    number = models.IntegerField(auto_created=True)
-    test = models.ForeignKey(Tests, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    tests = models.ManyToManyField(Tests)
 
+    def __str__(self):
+        return self.name
 
 class Attempts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     testing = models.ForeignKey(Testing, on_delete=models.CASCADE)
     answer = models.ManyToManyField(Answers)
+
+    def __str__(self):
+        return self.user.username + ': ' + self.testing.name
