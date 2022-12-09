ac96b5a008534a0bd95194c35e6625f14662831f
dmiv <dmitriy40647274@gmail.com>
2022-12-09 16:30:50 +0300

    test validation is work

--- testing/admin.py
+++ testing/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django import forms
 from .models import *
 
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Questions
+        fields = '__all__'
+
+    def clean(self):
+        if self.cleaned_data.get('question') == 'dmiv':
+            raise forms.ValidationError('name incorrect')
+        return self.cleaned_data
 
 @admin.register(Answers)
 class AnswersAdmin(admin.ModelAdmin):
@@ -10,6 +20,8 @@ class AnswersAdmin(admin.ModelAdmin):
 @admin.register(Questions)
 class QuestionsAdmin(admin.ModelAdmin):
     list_filter = ['testing']
+    form = QuestionForm
+
 
 
 admin.site.register(Testing)
