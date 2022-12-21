81f25e9302866a4e1e7276c3123057459c01353f
dmiv <dmitriy40647274@gmail.com>
2022-12-21 23:31:47 +0300

    add requirements.txt

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='index'),
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
--- testing/views.py
+++ testing/views.py
@@ -15,7 +15,7 @@ class TestingDetailView(DetailView):
     model = Testing
 
 
-def get_question(pk, user):
+def get_question(pk):
     question = Attempts.objects.filter(answer=None).first().question
     return question
 
@@ -28,15 +28,11 @@ def creating_attempt(user, testing):
 def test(request, pk):
     context = {}
     testing = Testing.objects.prefetch_related('questions__variants').get(id=pk)
-    print(len(Attempts.objects.filter(user_id=request.user.id, testing_id=pk, answer=None)) == 0)
-    print(len(Attempts.objects.filter(user_id=request.user.id, testing_id=pk)) == 0)
-    print(Attempts.objects.filter(user_id=request.user.id, testing_id=pk))
-    print(Attempts.objects.filter(user_id=request.user.id, testing_id=pk, answer=None))
     if len(Attempts.objects.filter(user_id=request.user.id, testing_id=pk)) == 0:
         creating_attempt(request.user, testing)
     elif len(Attempts.objects.filter(user_id=request.user.id, testing_id=pk, answer=None)) == 0:
         return redirect('./statistic')
-    question = get_question(pk, request.user)
+    question = get_question(pk)
     variants = question.variants.all()
     if request.POST:
         number_question = int(request.POST.get('number'))
@@ -45,7 +41,7 @@ def test(request, pk):
         attempt.answer.add(*answer)
     if len(Attempts.objects.filter(user_id=request.user.id, testing_id=pk, answer=None)) == 0:
         return redirect('./statistic')
-    question = get_question(pk, request.user)
+    question = get_question(pk)
     context['question'] = question
     context['answers'] = question.variants.all()
     return render(request, 'testing/passing.html', context=context)
@@ -63,20 +59,3 @@ def statistic(request, pk):
     else:
         statistic_obj = Statistic.objects.filter(user=request.user, testing_id=pk).last()
         return render(request, 'testing/statistic.html', {'statistic': statistic_obj})
-
-
-
-
-
-    # try:
-    #     statistic_obj = Statistic.objects.get(user=request.user, testing_id=pk)
-    #     return render(request, 'testing/statistic.html', {'statistic': statistic_obj})
-    # except Statistic.DoesNotExist:
-    #     attempt = Attempts.objects.filter(user=request.user, testing_id=pk)
-    #     correct = [(list(a) == list(b)) for a, b in zip([i.answer.all() for i in attempt], [i.question.correct.all() for i in attempt])]
-    #     count = sum(correct)
-    #     wrong = len(attempt.all()) - count
-    #     statistic_obj = Statistic.objects.create(user=request.user, testing_id=pk, correct=count, wrong=wrong)
-    #     [i.delete() for i in attempt]
-    #     return render(request, 'testing/statistic.html', {'statistic': statistic_obj})
-
